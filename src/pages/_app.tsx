837d0e29604aceb3b0c69b49345f799139683167
Javier Lorenzo Fdez <javier@theseedsquad.com>
2023-04-29 17:18:33 +0200

    add: router to custom uri with posdcast detail page and css refactor

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
+import { useRouter } from 'next/router';
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  return <Component {...pageProps} router={router} />
 }
